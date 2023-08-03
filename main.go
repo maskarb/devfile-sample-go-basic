d51fb823c8d5ee03437187b34fe3863039a773e8
maskarb <mskarbek@redhat.com>
2023-08-03 16:30:48 -0400

    write 2 endpoint handlers

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 var port = os.Getenv("PORT")
@@ -12,15 +13,16 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	http.HandleFunc("/api/prometheus/api/v1", HelloServer)
+	http.HandleFunc("/api/prometheus/api/v1/query", QueryServer)
+	http.HandleFunc("/api/prometheus/api/v1/query_range", QueryRangeServer)
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path[5:]
-	if path != "query_range" {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	} else {
-		fmt.Fprint(w, "Hello World!")
-	}
+func QueryServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
+
+func QueryRangeServer(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(11 * time.Second)
+	fmt.Fprint(w, "Hello World!")
 }
